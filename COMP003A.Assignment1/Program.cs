915172c0d5cf6e2662e14c5f25ae518928b50b99
Owner <Owner@DESKTOP-51DH0RO>
2023-01-20 03:29:47 -0800

    Add project files.

---
+++ COMP003A.Assignment1/Program.cs
@@ -0,0 +1,212 @@
+﻿// this is a single comment
+/* this is a multi-line comment
+*
+* Author: Lionel Villanueva
+* Course: COMP-001A-L01
+* Purpose: Code for Assignmeent 1 
+*/
+namespace COMP003A.Assignment1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("100 Things about me");
+            Console.WriteLine("1.  I like to eat canees");
+            Console.WriteLine("2.  I like to go to Mexico");
+            Console.WriteLine("3.  My favorite national team is Mexico");
+            Console.WriteLine("4.  I like the drive in the highway");
+            Console.WriteLine("5.  I want to leearn more about coding");
+            Console.WriteLine("6.  I enjoy visiting my grandma in winter break");
+            Console.WriteLine("7.  Sopes is the best food to ever exist ");
+            Console.WriteLine("8.  I like to watch movies when theey release");
+            Console.WriteLine("9.  I would like to traveel more");
+            Console.WriteLine("10. I spend a lot of my time playing video games with friends");
+            Console.WriteLine("11. I also enjoy watching streams");
+            Console.WriteLine("12. I enjoy watching a bit of anime");
+            Console.WriteLine("13. I like to play basketball with my family members");
+            Console.WriteLine("14. One of my favorite movies is the Maze Runner");
+            Console.WriteLine("15. I like to met new people");
+            Console.WriteLine("16. I like to learn new things");
+            Console.WriteLine("17. I enjoy playing fps games which are hard ");
+            Console.WriteLine("18. I like to listen to music when Im doing something ");
+            Console.WriteLine("19. I am 5'10");
+            Console.WriteLine("20. My birthday is on May 21");
+            Console.WriteLine("21. One of my high school teachers got me into coding ");
+            Console.WriteLine("22. My first ticket was speeding in a 55");
+            Console.WriteLine("23. I have some roofing experience");
+            Console.WriteLine("24. I like to build legos");
+            Console.WriteLine("25. I dont like soda");
+            Console.WriteLine("26. I have never touched snow");
+            Console.WriteLine("27. I like to eat beans and tortillas");
+            Console.WriteLine("28. I would like to sky divve some day ");
+            Console.WriteLine("29. I like spending my money on dumb things");
+            Console.WriteLine("30. Canelo is my favorite boxer");
+            Console.WriteLine("31. I like to work with my parents ");
+            Console.WriteLine("32. I am a picky eater");
+            Console.WriteLine("33. I used to have a coyote");
+            Console.WriteLine("34. I went to an art museum in middle school");
+            Console.WriteLine("35. Dogs are better than cats");
+            Console.WriteLine("36. Chicharito was one of the best mexican soccer player");
+            Console.WriteLine("37. The gold cup 2011 final was one of the best games to ever watch ");
+            Console.WriteLine("38. One of the most intensee football games wwas the Rabens vs the 49ers when both head coaches were brothers");
+            Console.WriteLine("39. I like to eat cereal");
+            Console.WriteLine("40. I played a bunch of supercell game in my middle school years");
+            Console.WriteLine("41. Fruit Punch Brisk is my favorite drink ");
+            Console.WriteLine("42. I like to listen to spanish rap");
+            Console.WriteLine("43. I like driving dirt bikes in Mexico ");
+            Console.WriteLine("44. Offroading is a very fun thing to do with friends");
+            Console.WriteLine("45. I like to eat pancakes in the morning");
+            Console.WriteLine("46. I enjoy listening to Corridos when Im driving somewhere");
+            Console.WriteLine("47. I like to have conversations every day with my mom");
+            Console.WriteLine("48. Me and my brother havee a passion for whos better at a game ");
+            Console.WriteLine("49. I like to sleep late then regret it");
+            Console.WriteLine("50. I like squishmallows");
+            Console.WriteLine("51. I like watching tik toks ");
+            Console.WriteLine("52. I like the 49ers");
+            Console.WriteLine("53. Grant is one of my favorite teachers");
+            Console.WriteLine("54. Yugioh was one of the best animes");
+            Console.WriteLine("55. I like spending time with my brothers");
+            Console.WriteLine("56. I like watching yard sale or flea market videos");
+            Console.WriteLine("57. I am the older out of my 3 brothers  and 1 sister");
+            Console.WriteLine("58. One of my favorite youtubers is Biaheza");
+            Console.WriteLine("59. The game isnt over if the pawn took your queen only  until the king has fallen ");
+            Console.WriteLine("60. My major is computer science");
+            Console.WriteLine("61. I like the Diary of the Wimpy kid novals");
+            Console.WriteLine("62. I like to play chess but not that smart with the moves");
+            Console.WriteLine("63. I have a 1650 gpu");
+            Console.WriteLine("64. I like watching Marval Movies");
+            Console.WriteLine("65. I like to bait my teammates when playing fps games ");
+            Console.WriteLine("66. I got my license a month after my first ticket");
+            Console.WriteLine("67. I have never smoked");
+            Console.WriteLine("68. I would pick up bottles for money when I was younger");
+            Console.WriteLine("69. I like to explore nature");
+            Console.WriteLine("70. I hate pineapple on my pizza ");
+            Console.WriteLine("71. I enjoy watching rust videos");
+            Console.WriteLine("72. I like looking at pc components");
+            Console.WriteLine("73. Trying to learn how to play the accordian");
+            Console.WriteLine("74. I like to watch Curious George");
+            Console.WriteLine("75. I like to eat tamales for christmas");
+            Console.WriteLine("76. I try to keep my room clean");
+            Console.WriteLine("77. I multitask a lot when it comes to homework");
+            Console.WriteLine("78. I dont like strawberries");
+            Console.WriteLine("79. I like when it rains since everyone just admires it");
+            Console.WriteLine("80. Ghosting everyone might be good ");
+            Console.WriteLine("81. I am taking Calculus this semester");
+            Console.WriteLine("82. Lays are top tier chips ");
+            Console.WriteLine("83. Oreos are my favorite cookies");
+            Console.WriteLine("84. I like to play board games with family ");
+            Console.WriteLine("85. We always make a lot of food on holidays ");
+            Console.WriteLine("86. I want to move to Oregon with my girlfrieend");
+            Console.WriteLine("87. I want to have a big piece of land in Mexico");
+            Console.WriteLine("88. I want to learn multiple coding languagues");
+            Console.WriteLine("89. I enjoy eating icce cream ");
+            Console.WriteLine("90. I got baptized with my sister");
+            Console.WriteLine("91. I never played sport in school");
+            Console.WriteLine("92. I liked playuing soccer but no anymore");
+            Console.WriteLine("93. Ramen noodles are a must each week");
+            Console.WriteLine("94. Chochlate covered pretzels are the best thing");
+            Console.WriteLine("95. My dream car is a corvette");
+            Console.WriteLine("96. I would like to own a g wagon or a tesla some day ");
+            Console.WriteLine("97. I like wrestling in the jumping house");
+            Console.WriteLine("98. Dont really like going to the beach");
+            Console.WriteLine("99. I like to go hunting");
+            Console.WriteLine("100. I have always been afraid of clowns");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
